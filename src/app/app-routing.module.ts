59b6575ee50a11429bd59b7ec68d849d68856555
sentaaa <mueller.senta1001@gmail.com>
2020-04-09 08:54:23 +0200

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListBooksComponent } from './components/list-books/list-books.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "listBooks",
+component: ListBooksComponent,
+
+}, {path:"",
+component: HomeComponent,}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
